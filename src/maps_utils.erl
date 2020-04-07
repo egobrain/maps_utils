278385a0a4253a54c4b861be01603685fb7977e4
Tamas Kelliar <tamas.kelliar@otpbank.hu>
2020-04-07 11:04:34 +0200

    PE-1801: Counter overwrite function

--- src/maps_utils.erl
+++ src/maps_utils.erl
@@ -9,6 +9,7 @@
          filter/2,
          filtermap/2,
          diff/2,
+         diff/3,
          update/4
         ]).
 
@@ -55,26 +56,57 @@ filtermap(Fun, Map) ->
     end, #{}, Map).
 
 diff(From, To) ->
-    lists:reverse(diff(From, To, [], [])).
+  lists:reverse(diff(From, To, [], [])).
 
 diff(From, To, Path, Log) when is_map(From), is_map(To) ->
-    FromKeys = maps:keys(From),
-    NewPairs = maps:to_list(maps:without(FromKeys, To)),
-    {Log2, NewPairs2} = maps:fold(
-        fun(K, FromV, {L, New}) ->
-            case maps:find(K, To) of
-                {ok, ToV} -> {diff(FromV, ToV, [K|Path], L), New};
-                error -> maybe_moved(K, FromV, NewPairs, Path, L)
-            end
-        end, {Log, NewPairs}, From),
-    lists:foldl(fun({K, V}, L) ->
-        [#{op => add, path => path([K|Path]), value => V}|L]
-    end, Log2, NewPairs2);
+  FromKeys = maps:keys(From),
+  NewPairs = maps:to_list(maps:without(FromKeys, To)),
+  {Log2, NewPairs2} = maps:fold(
+    fun(K, FromV, {L, New}) ->
+      case maps:find(K, To) of
+        {ok, ToV} -> {diff(FromV, ToV, [K|Path], L), New};
+        error -> maybe_moved(K, FromV, NewPairs, Path, L)
+      end
+    end, {Log, NewPairs}, From),
+  lists:foldl(fun({K, V}, L) ->
+    [#{op => add, path => path([K|Path]), value => V}|L]
+              end, Log2, NewPairs2);
 diff(From, To, Path, Log) when is_list(From), is_list(To) ->
-    list_diff(From, To, Path, Log, 0);
+  list_diff(From, To, Path, Log, 0);
 diff(From, To, _Path, Log) when From =:= To -> Log;
 diff(_From, To, Path, Log) ->
-    [#{op => replace, path => path(Path), value => To}|Log].
+  [#{op => replace, path => path(Path), value => To}|Log].
+
+diff(From, To, Fun) ->
+    lists:reverse(diff(From, To, [], [], Fun)).
+
+diff(From, To, Path, Log, Fun) when is_map(From), is_map(To) ->
+  FromKeys = maps:keys(From),
+  NewPairs = maps:to_list(maps:without(FromKeys, To)),
+  {Log2, NewPairs2} = maps:fold(
+    fun(K, FromV, {L, New}) ->
+      case maps:find(K, To) of
+        {ok, ToV} ->
+          {diff(FromV, ToV, [K | Path], L, Fun), New};
+        error ->
+          maybe_moved(K, FromV, NewPairs, Path, L)
+      end
+    end, {Log, NewPairs}, From),
+  lists:foldl(fun({K, V}, L) ->
+                [#{op => add, path => path([K | Path]), value => V} | L]
+              end, Log2, NewPairs2);
+diff(From, To, Path, Log, Fun) when is_list(From), is_list(To) ->
+  list_diff(From, To, Path, Log, 0, Fun);
+diff(From, To, _Path, Log, _Fun) when From =:= To ->
+  Log;
+diff(From, To, Path, Log, Fun) when is_binary(From), is_binary(To) ->
+  Fun(From, To, path(Path), Log);
+diff(From, To, Path, Log, Fun) when is_integer(From), is_integer(To), To < From ->
+  Fun(From, To, path(Path), Log);
+diff(From, To, Path, Log, Fun) when is_integer(From), is_integer(To), To > From ->
+  Fun(From, To, path(Path), Log);
+diff(_From, To, Path, Log, _Fun) ->
+  [#{op => replace, path => path(Path), value => To}|Log].
 
 update(Key, Map, New, UpdateFun) ->
     maps:put(Key,
@@ -86,9 +118,9 @@ update(Key, Map, New, UpdateFun) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-
 maybe_moved(K, FromV, Pairs, Path, L) ->
     maybe_moved_(K, FromV, Pairs, Path, L, []).
+
 maybe_moved_(K, _V, [], Path, Log, Acc) ->
     {[#{op => remove, path => path([K|Path])}|Log], Acc};
 maybe_moved_(K, V, [{NewK, V}|Rest], Path, Log, Acc) ->
@@ -98,11 +130,21 @@ maybe_moved_(K, V, [Other|Rest], Path, Log, Acc) ->
     maybe_moved_(K, V, Rest, Path, Log, [Other|Acc]).
 
 list_diff([From|RestF], [To|RestT], Path, Log, Cnt) ->
-    list_diff(RestF, RestT, Path, diff(From, To, [Cnt|Path], Log), Cnt+1);
+  list_diff(RestF, RestT, Path, diff(From, To, [Cnt|Path], Log), Cnt+1);
 list_diff([_|Rest], [], Path, Log, Cnt) ->
-    NewLog = [#{op => remove, path => path([Cnt|Path])}|Log],
-    list_diff(Rest, [], Path, NewLog, Cnt+1);
+  NewLog = [#{op => remove, path => path([Cnt|Path])}|Log],
+  list_diff(Rest, [], Path, NewLog, Cnt+1);
 list_diff([], Rest, Path, Log, _Cnt) ->
+  lists:foldl(fun(V, L) ->
+    [#{op => add, path => path(["-"|Path]), value => V}|L]
+              end, Log, Rest).
+
+list_diff([From|RestF], [To|RestT], Path, Log, Cnt, Fun) ->
+    list_diff(RestF, RestT, Path, diff(From, To, [Cnt|Path], Log, Fun), Cnt+1, Fun);
+list_diff([_|Rest], [], Path, Log, Cnt, Fun) ->
+    NewLog = [#{op => remove, path => path([Cnt|Path])}|Log],
+    list_diff(Rest, [], Path, NewLog, Cnt+1, Fun);
+list_diff([], Rest, Path, Log, _Cnt, Fun) ->
     lists:foldl(fun(V, L) ->
         [#{op => add, path => path(["-"|Path]), value => V}|L]
     end, Log, Rest).
@@ -205,6 +247,52 @@ diff_test_() ->
          diff(#{a => 1, b => [1, #{c => 3}, 7], d => 4},
               #{e => 1, b => [2, #{c => 4}], d => 4, k => #{l => 1}})))
     ].
+
+diff_nums_test_() ->
+  Fun = fun(_, To, _, Log) -> [#{fun_result => To} | Log] end,
+  [
+    ?_test(?assertEqual([], diff(#{a => 1}, #{a => 1}, Fun))),
+    ?_test(?assertEqual([#{fun_result => 2}], diff(#{a => 1}, #{a => 2}, Fun))),
+    ?_test(?assertEqual(
+      [
+        #{ op => move, path => <<"/b">>, from => <<"/a">>}
+      ],
+      diff(#{a => 1}, #{b => 1}, Fun))),
+    ?_test(?assertEqual(
+      [
+        #{ op => add, path => <<"/a">>, value => 1}
+      ],
+      diff(#{}, #{a => 1}, Fun))),
+    ?_test(?assertEqual(
+      [
+        #{ op => remove, path => <<"/a">>}
+      ],
+      diff(#{a => 1}, #{}, Fun))),
+    ?_test(?assertEqual(
+      [
+        #{op => remove,path => <<"/a">>},
+        #{op => add,path => <<"/c">>,value => 3},
+        #{op => add,path => <<"/b">>,value => 2}
+      ],
+      diff(#{a => 1}, #{b => 2, c => 3}, Fun))),
+    ?_test(?assertEqual(
+      [#{from => <<"/a">>,op => move,path => <<"/e">>},
+       #{fun_result => 2},
+       #{fun_result => 4},
+       #{op => add,path => <<"/b/-">>,value => 7},
+       #{op => add,path => <<"/k">>,value => #{l => 1}}],
+      diff(#{a => 1, b => [1, #{c => 3}], d => 4},
+           #{e => 1, b => [2, #{c => 4}, 7], d => 4, k => #{l => 1}}, Fun))),
+
+    ?_test(?assertEqual(
+      [#{fun_result => 2},
+       #{op => replace,path => <<"/b">>,value => y},
+       #{fun_result => 3},
+       #{fun_result => <<"3">>},
+       #{op => remove,path => <<"/e/1">>}],
+      diff(#{a => 1, b => x, c => 4, d => <<"4">>, e => [1, 2]},
+           #{a => 2, b => y, c => 3, d => <<"3">>, e => [1]}, Fun)))
+  ].
 
 merge_with_test_() ->
     [
